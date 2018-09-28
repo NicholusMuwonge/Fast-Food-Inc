33f7357ce7a047a6608f757cf9f1fac607a3cd0e
nicholusmuwonge <nicholusmuwonge@gmail.com>
2018-09-28 11:18:25 +0300

    changes

--- api-v1/run.py
+++ api-v1/run.py
@@ -1,17 +1,25 @@
-from flask import Flask, Response,abort, request, redirect, make_response,jsonify
+from flask import Flask, Response,abort, request, make_response,jsonify
 import json 
 from App.utils import search_orders, modify_helper 
-app = Flask(__name__)
+
+app = Flask(__name__) 
+#Flaskis a class in module flask that contains extensions that ensure the app runs properly as it shouldand the name is right coz its initial imported application name is different from main so its a place holder
+
 
 orders = [] 
+#we start with an empty list of dictionaries
+#our index route
+
 @app.route("/api/v1/")
 def index():
     return "Fast Foods App"
 
+#our route for getting all orders
 @app.route("/api/v1/orders", methods= ['GET'])
 def get_all_orders():
     return make_response(jsonify(orders)),200
 
+#our route for getting anly one order
 @app.route("/api/v1/orders/<int:order_id>", methods= ['GET'])
 def fetch_one_order(order_id):
     order = search_orders(orders, order_id)
@@ -20,32 +28,36 @@ def fetch_one_order(order_id):
     else:
         return make_response(jsonify(order)),200
 
+#our route for placing an order
 @app.route("/api/v1/orders", methods= ['POST'])
 def place_order():
-    data = request.json
+    data = request.json #picks data sent from json
     if len(orders) == 0:
-        order_id = 1
+        order_id = 1   #initializes the first order with order_id 1  
         orders.append({'order_id' : order_id,'user_id': data.get('user_id'),'item_ordered': data.get('item_ordered'),
               'item_quantity':data.get('item_quantity') })
+              #adds whatever was sent from postman to orders, using keys from the data object
     else:
         last_dict = orders[len(orders)-1]
+        #this picks the last order in the orders list and gets its id which we add onto 1 to auto increment it for the next order
         id = int(last_dict['order_id'])
         new_id = id + 1 
         orders.append({'order_id' : new_id, 'user_id' : data.get('user_id'),'item_ordered' : data.get('item_ordered'), 'item_quantity': data.get('item_quantity')} )
     
     return make_response(jsonify(orders)),201
-
+#modify method.
 @app.route("/api/v1/orders/<int:order_id>", methods= ['PUT'])
 def modify_order(order_id):
      data = request.json
      if data:
          status = data.get('status')
          response =  modify_helper(orders,order_id,status)
-         return  make_response(jsonify(response)),200
+         #modify_helper is a function in utils.py, it helps us modify the order with the status key and value
+         return  make_response(jsonify(response)),201
 
      
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':# checks to c if the whole module runs properly
     app.run(debug=True)
--- api-v1/test_api.py
+++ api-v1/test_api.py
@@ -1,19 +1,19 @@
-import unittest,json   
-from flask import request, jsonify
+import unittest, json
 from run import app
 
-class test_api(unittest.TestCase):
+class test_api(unittest.TestCase):#defn of your own tests
 
     @classmethod
     def setUpClass(self):  
         print('SetUp')
         self.orders = {"user_id": "U1", "item_ordered": "chicken and beef","item_quantity":"2kg"}      
-        self.client = app.test_client()
+        self.client = app.test_client() #a flask test_client() gives you  access tp get,post and put methods that help route to differnet URLs
 	    
     @classmethod
     def tearDownClass(self):
         print('TearDown')
-                
+
+    #the setUpClass and tearDownClass only run once since they are class methods                    
 
     def test_init_page(self):
         rv = self.client.get('/api/v1/')
@@ -41,9 +41,11 @@ class test_api(unittest.TestCase):
     def test_modify_order(self):
         rv = self.client.post('/api/v1/orders', data=json.dumps(self.orders), content_type="application/json")
         self.assertEqual(rv.status_code, 201)
-        rv2 = self.client.put('/api/v1/orders/1', json={'status':'completed'}, content_type="application/json")
-        self.assertEqual(rv2.status_code, 200)
+        rv2 = self.client.put('/api/v1/orders/1', data= json.dumps({'status':'completed'}), content_type="application/json")
+        self.assertEqual(rv2.status_code, 201)
          
 
 if __name__ == "__main__":
     unittest.main()
+
+# python -m unittest test_api.py
